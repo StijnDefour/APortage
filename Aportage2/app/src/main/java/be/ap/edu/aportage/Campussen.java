69bf97d08200ac114f45eec7295312ffb4d13a85
karima-elhachimi <yuakarima@Yuas-MacBook-Pro.local>
2018-11-16 16:08:14 +0100

    recyclerview aangemaakt, todo: viewholder aanmaken

--- Aportage2/app/src/main/java/be/ap/edu/aportage/Campussen.java
+++ Aportage2/app/src/main/java/be/ap/edu/aportage/Campussen.java
@@ -4,17 +4,30 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class Campussen extends AppCompatActivity {
 
+    private RecyclerView mijnCampussenRV;
+    private GridLayoutManager mijnGridLM;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campussen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //initialisatie properties voor recyclerview van campussen
+        this.mijnCampussenRV = (RecyclerView) findViewById(R.id.rv_campussen);
+        this.mijnGridLM = new GridLayoutManager(getApplicationContext(), 3, LinearLayoutManager.HORIZONTAL,false);
+        this.mijnCampussenRV.setLayoutManager(this.mijnGridLM);
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
