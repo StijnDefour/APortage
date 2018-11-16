32bedc79184844bd79373d6a42b48242e8cca62e
karima-elhachimi <yuakarima@Yuas-MacBook-Pro.local>
2018-11-16 14:03:10 +0100

    views aangemaakt op basis van de wireframes

---
+++ Aportage2/app/src/main/java/be/ap/edu/aportage/Melding.java
@@ -0,0 +1,14 @@
+package be.ap.edu.aportage;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Melding extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_melding);
+    }
+
+}
