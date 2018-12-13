85d7128b1e22d93d0d66d266beec2e8b3e787a62
Mahyar Moghimi Kian <mahyar.kian@gmail.com>
2018-12-13 15:41:26 +0100

    bug: routing doesn't work

--- Aportage-dashboard/src/app/app-routing.module.ts
+++ Aportage-dashboard/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { CampusComponent } from './campus/campus.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'campussen', component: CampusComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ Aportage-dashboard/src/app/campus/campus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-campus',
+  templateUrl: './campus.component.html',
+  styleUrls: ['./campus.component.css']
+})
+export class CampusComponent {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
