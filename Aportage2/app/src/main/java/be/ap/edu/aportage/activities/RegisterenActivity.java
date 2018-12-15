92e436396fc8177b47d22d5ae7f4bc14890d28fa
karima-elhachimi <yuakarima@Yuas-MacBook-Pro.local>
2018-12-15 12:26:03 +0100

    login en registratie layout, done, scrollview toegevoegd

---
+++ Aportage2/app/src/main/java/be/ap/edu/aportage/activities/RegisterenActivity.java
@@ -0,0 +1,15 @@
+package be.ap.edu.aportage.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import be.ap.edu.aportage.R;
+
+public class RegisterenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registeren);
+    }
+}
